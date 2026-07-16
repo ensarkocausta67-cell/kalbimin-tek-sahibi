1483489e88a8f0ea4be811110a3dbe00a2cd0cfc
ensarkocausta67-cell <ensarkocausta67@icloud.com>
2026-07-16 22:33:46 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+const loginPage = document.querySelector(".login-page");
+const mainPage = document.getElementById("mainPage");
+const error = document.getElementById("error");
+
+function login() {
+
+    const password = document
+        .getElementById("password")
+        .value
+        .trim()
+        .toLowerCase();
+
+    if (
+        password === "kalbimin tek sahibi" ||
+        password === "kalbiminteksahibi"
+    ) {
+
+        loginPage.style.opacity = "0";
+
+        setTimeout(() => {
+
+            loginPage.style.display = "none";
+            mainPage.style.display = "block";
+
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+
+        },700);
+
+    }
+
+    else{
+
+        error.innerHTML="❤️ Şifre yanlış...";
+
+    }
+
+}
